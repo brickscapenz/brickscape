6f03964a5a047533a12c33ae6a2a1ea0c1cba7c1
Brick scape <brickscapenz@gmail.com>
2018-10-13 19:19:34 +1300

    Add hero image section for home page cms

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -6,6 +6,7 @@ const HomePagePreview = ({ entry, widgetFor }) => (
   <HomePageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
+    image={entry.getIn(['data', 'image'])}
   />
 )
 

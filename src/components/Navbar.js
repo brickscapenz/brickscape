3347decddcbaf6e19f6dd2c884676a0c8e471dd4
Brick scape <brickscapenz@gmail.com>
2018-10-23 12:38:42 +1300

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,21 @@ import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className="navbar navbar-expand-lg navbar-light">
     <div className="container">
       <div className="row">
-        <Link className="nav-link text-dark" to="/">
-          <figure className="image">
-            <h1>Brickscape</h1>
-          </figure>
-        </Link>
-
+        <div>
+          <Link className="nav-link text-dark" to="/">
+            <figure className="image">
+              <h1>Brickscape</h1>
+            </figure>
+          </Link>
+        </div>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li class="nav-item">
               <Link className="nav-link" to="/projects">
-                Projects
+                Gallery
         </Link>
             </li>
             <li class="nav-item">

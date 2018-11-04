9f078770f5f4941387365e374fb3b973e072321e
Daniel Peek <Daniel.Peek@enlighten.co.nz>
2018-11-05 12:56:19 +1300

    Add dependencies for bootstrap navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,32 +2,36 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light">
     <div className="container">
       <div className="row d-flex justify-content-between" style={{ width: '100%'}}>
         <div>
-          <Link className="nav-link text-white" to="/">
+          <Link className="nav-link text-black" to="/">
             <figure className="image">
-              <h1>Brickscape</h1>
+              <h1><strong>Brickscape</strong></h1>
             </figure>
           </Link>
         </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse d-flex justify-content-end" style={{ 'margin-right': '-20px'}} id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/projects">
+              <Link className="nav-link" to="/projects">
                 Gallery
         </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/about">
+              <Link className="nav-link" to="/about">
                 About
         </Link>
             </li>
             <li className="nav-item pr-0">
-              <Link className="nav-link text-white" to="/contact">
+              <Link className="nav-link" to="/contact">
                 Contact
         </Link>
             </li>

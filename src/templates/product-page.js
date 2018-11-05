9e6896f4d15aa4b561046ef3dd768dcbbe5c66c2
Daniel Peek <Daniel.Peek@enlighten.co.nz>
2018-11-05 16:56:15 +1300

    Pull services data from admin

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -6,16 +6,15 @@ import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
 import Pricing from '../components/Pricing'
 
+const AllServices = (services) = {
+}
+
 export const ProductPageTemplate = ({
   image,
   title,
   heading,
   description,
-  intro,
-  main,
-  testimonials,
-  fullImage,
-  pricing,
+  services,
 }) => (
   <div className="container-fluid">
     <div className="row services-row">
@@ -38,23 +37,7 @@ ProductPageTemplate.propTypes = {
   title: PropTypes.string,
   heading: PropTypes.string,
   description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-  main: PropTypes.shape({
-    heading: PropTypes.string,
-    description: PropTypes.string,
-    image1: PropTypes.object,
-    image2: PropTypes.object,
-    image3: PropTypes.object,
-  }),
   testimonials: PropTypes.array,
-  fullImage: PropTypes.string,
-  pricing: PropTypes.shape({
-    heading: PropTypes.string,
-    description: PropTypes.string,
-    plans: PropTypes.array,
-  }),
 }
 
 const ProductPage = ({ data }) => {
@@ -67,11 +50,7 @@ const ProductPage = ({ data }) => {
         title={frontmatter.title}
         heading={frontmatter.heading}
         description={frontmatter.description}
-        intro={frontmatter.intro}
-        main={frontmatter.main}
-        testimonials={frontmatter.testimonials}
-        fullImage={frontmatter.full_image}
-        pricing={frontmatter.pricing}
+        services={frontmatter.services}
       />
     </Layout>
   )
@@ -94,45 +73,10 @@ export const productPageQuery = graphql`
         title
         image
         heading
-        description
-        intro {
-          blurbs {
-            image
-            text
-          }
-          heading
-          description
-        }
-        main {
-          heading
-          description
-          image1 {
-            alt
-            image
-          }
-          image2 {
-            alt
-            image
-          }
-          image3 {
-            alt
-            image
-          }
-        }
+        description      
         testimonials {
-          author
-          quote
-        }
-        full_image
-        pricing {
-          heading
+          image
           description
-          plans {
-            description
-            items
-            plan
-            price
-          }
         }
       }
     }
